6fdc9d83b97803c5bcc683d82afb1087c0a4d21c
Ronit <maity.ronit18@gmail.com>
2026-02-15 11:32:11 +0530

    all origins

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 // ✅ ENABLE CORS FOR EXPRESS
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   })
 );
@@ -67,7 +67,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
